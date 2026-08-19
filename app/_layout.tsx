126d835a865b446070e0985eb671276713a4d558
Anish Narendra Metha Palsamy <anishnarendrametha.p@zynith-it.com>
2026-08-19 16:41:56 +0530

    Overall layout updated

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,22 @@
 import { Stack } from 'expo-router';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { AuthProvider } from '../service/auth';
 import { QuestionnaireProvider } from '../contexts/QuestionnaireContext';
 
 export default function RootLayout() {
   return (
-    <AuthProvider>
-      <QuestionnaireProvider>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="index" />
-          <Stack.Screen name="(auth)" />
-          <Stack.Screen name="(app)" />
-        </Stack>
-      </QuestionnaireProvider>
-    </AuthProvider>
+    <SafeAreaProvider>
+      <SafeAreaView edges={['top']} style={{ flex: 1, paddingTop: 16, backgroundColor: '#090B0C' }}>
+        <AuthProvider>
+          <QuestionnaireProvider>
+            <Stack screenOptions={{ headerShown: false }}>
+              <Stack.Screen name="index" />
+              <Stack.Screen name="(auth)" />
+              <Stack.Screen name="(app)" />
+            </Stack>
+          </QuestionnaireProvider>
+        </AuthProvider>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
